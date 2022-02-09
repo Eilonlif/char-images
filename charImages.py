4e629c66b0bd004cec19f5a35931561392351a9e
Eilonlif <43208668+Eilonlif@users.noreply.github.com>
2022-02-10 01:06:28 +0200

    Added terminal functionality

--- charImages.py
+++ charImages.py
@@ -2,10 +2,11 @@ from math import gcd
 
 import numpy as np
 from PIL import Image
+import sys
 
 
 class image:
-    def __init__(self, image, x1: int, y1: int, x0: int = 0, y0: int = 0, step: int = None, chars=None):
+    def __init__(self, image, x1: int, y1: int, step: int = None, x0: int = 0, y0: int = 0, chars=None):
         im = np.array(image, dtype="uint8")
         self.height = y1 - y0
         self.width = x1 - x0
@@ -19,6 +20,9 @@ class image:
                 f"Something is wrong, please re-enter values\nDimensions are: {len(im[0])}x{len(im)}, step={self.step}")
         self.image = im[y0: y1, x0: x1]
 
+    def __str__(self):
+        return '\n'.join(' '.join(l) for l in self.image.tolist())
+
     def check_image(self, pich: int, picw: int):
         return picw >= self.width > 0 and pich >= self.height > 0 and self.height % self.step == 0 == self.width % self.step
 
@@ -30,7 +34,7 @@ class image:
 
     def chunks(self):
         def avg(x, i, j, s):
-            return x[i: i + s, j: j + s].mean()
+            return np.average(x[i: i + s, j: j + s])
 
         ret = [([avg(self.image, i, j, self.step) for j in range(0, self.width, self.step)]) for i in
                range(0, self.height, self.step)]
@@ -41,9 +45,6 @@ class image:
             return np.array([chars[int(a / 255.0 * len_chars)] for a in x])
 
         return np.apply_along_axis(lambda x: to_chr_help(x, self.chars, len(self.chars)), 0, np.array(self.image))
-
-    def print_image(self):
-        [print(' '.join(l)) for l in self.image.tolist()]
 
     def write_to_file(self, file_name: str):
         with open(f"{file_name}.txt", 'w') as f:
@@ -61,14 +62,58 @@ class image:
         self.image = self.generate_image()
 
 
-x0 = 200
-y0 = 0
-x1 = 1100
-y1 = 1290
-step = 10
-fname = "eilonimg.png"
+def user_input(im):
+    user_in = ''
+    option_dictionary = dict([("print", "print the image"), ("write", "write image to a file"), ("exit", "exit"),
+                              ("error", "Did not match anything, enter again!")])
+    while user_in != "exit":
+        for i, k_v in enumerate(list(option_dictionary.items())[:-1]):
+            print(f"{i}) {k_v[0]}: {k_v[1]}")
+        user_in = input("Enter an option: ")
+        if user_input == "print":
+            print(im)
+        elif user_in == "write":
+            file_name = input("Enter file name")
+            print(f"Writing image to file: {file_name}")
+            im.write_to_file(file_name)
+        elif user_in == "exit":
+            print("Exiting")
+            quit()
+        else:
+            print(option_dictionary["error"])
 
-with Image.open(fname) as ima:
-    im = image(ima, x1=x1, y1=y1, step=step)
-im.convert()
-im.print_image()
+
+def user_input_handler_no_args():
+    input_list = ["x1", "y1", "step", "x0", "y0"]
+    values_list = []
+    for inp in input_list:
+        values_list.append(int(input(f"Enter value for {inp}: ")))
+    return values_list
+
+
+def open_image(file_name, args):
+    im = None
+    try:
+        with Image.open(file_name) as ima:
+            im = image(ima, *args)
+        im.convert()
+    except FileNotFoundError:
+        print("File Not Found!")
+    return im
+
+
+def main():
+    args = sys.argv
+    if len(args) == 1:
+        raise ValueError("Enter a file name!")
+    elif len(args) == 2:
+        im = open_image(args[1], user_input_handler_no_args())
+    else:
+        im = open_image(args[1], list(map(int,args[2:])))
+    if im is None:
+        raise ValueError("Something is wrong LOL!")
+    user_input(im)
+
+
+if __name__ == "__main__":
+    main()
